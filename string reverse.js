482a69ca0c0fc05aa2e86a0e222a6ae278ff48bd
cheerlarahul <cheerlarahul99@gmail.com>
2022-07-29 13:02:41 +0530

    updated

---
+++ string reverse.js
@@ -0,0 +1,16 @@
+
+//javascript
+
+// javascript String Reverse code 
+
+var string1="javascript"
+
+function stringreverse(str1){
+
+let reverse=str1.split("").reverse().join("")
+
+console.log(reverse);
+
+}
+
+stringreverse(string1);
