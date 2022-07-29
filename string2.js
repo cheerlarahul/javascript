35b49a2791f4624cc5764be00cc2f021677fe2ed
cheerlarahul <cheerlarahul99@gmail.com>
2022-07-29 20:21:03 +0530

    string reverse

--- string2.js
+++ string2.js
@@ -1,1 +1,13 @@
-let string2="Javascript string2";
+
+function strrev(str){
+
+for (let index = str.length-1; index >=0; index--) {
+    
+    let str2="";
+    str2+=str[index];
+}
+
+}
+
+
+strrev("javascript")
