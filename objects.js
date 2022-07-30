a6dde7be9b9829ae4bd298f42ae3a7c20e596e39
cheerlarahul <cheerlarahul99@gmail.com>
2022-07-30 10:33:40 +0530

    objects

--- objects.js
+++ objects.js
@@ -1,1 +1,55 @@
-#objects
+// object
+
+
+let obj1={
+    name:"Alex",
+    age:22,
+    gender:"Male",
+    place:"Hyderbad",
+    like:["food",'gaming','movies'],
+    adress:{
+        Street: "Z-20 Sector 12",
+        City: "Hyderabad",
+        StateFull: "India"
+    }
+}
+
+// object properties acessing (dot) notation
+
+console.log(obj1)
+
+console.log(obj1.like[0])
+
+console.log(obj1.adress.City)
+
+
+// object properties acessing [bracket] notation
+
+console.log(obj1.adress["Street"])
+
+console.log(obj1.adress["StateFull"])
+
+obj1.salary=50000;
+
+
+
+console.log(obj1.name="alexa");
+
+const{name,age,gender,}= obj1;
+
+const{name:n, age:a, gender:g}=obj1;
+
+console.log(name);
+console.log(obj1);
+
+
+console.log(n)
+
+const{code="XYZ"}=obj1;
+
+console.log(code)
+
+console.log(obj1)
+
+
+
